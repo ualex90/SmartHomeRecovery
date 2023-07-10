6e0769b6a218cfdc72a8ed1f659d08baf87e0e45
u_alex90 <u_alex90@mail.ru>
2023-07-10 23:34:32 +1000

    creat test_get_set_value

--- tests/test_modbus.py
+++ tests/test_modbus.py
@@ -1,8 +1,11 @@
-from config.config import CLIENTS, MODULES
+import pytest
+
+from config.test_data import TEST_MODULES
 from src.modbus.modbus import get_set_value
 from src.models.Dev import Dev
 
 
-def test_get_set_value():
-    device = Dev(CLIENTS[1], MODULES[0])
-    assert get_set_value(device) == [3, 260]
+@pytest.mark.parametrize("module", [i for i in TEST_MODULES])
+def test_get_set_value(module):
+    device = Dev({}, module)
+    assert get_set_value(device) == [module['unit_id'], module['result']]
