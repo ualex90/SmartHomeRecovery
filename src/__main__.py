d9ab6701d302ddf1b7de8371c190cd0eac73141e
u_alex90 <u_alex90@mail.ru>
2023-07-11 22:50:54 +1000

    Создан manual_request.py для ручного запроса данных с устройства

--- src/__main__.py
+++ src/__main__.py
@@ -1,16 +1,6 @@
-from config.config import CLIENTS, MODULES
-from src.modbus.modbus import read_scenarios, get_device_info
-from src.models.Dev import Dev
-
 
 def main():
-    device = Dev(CLIENTS[0], MODULES[7])
-    default = Dev(CLIENTS[0])
-
-    print(device, "\n")
-    print(get_device_info(device))
-
-    [print(i) for i in read_scenarios(device, 1)]
+    ...
 
 
 if __name__ == '__main__':
---
+++ src/manual_request.py
@@ -0,0 +1,18 @@
+from config.config import CLIENTS, MODULES
+from src.modbus.modbus import read_scenarios, get_device_info
+from src.models.Dev import Dev
+
+# Создание объекта модуля (данные клиента tcp, данные модуля)
+device = Dev(CLIENTS[0], MODULES[7])
+
+# Создание объекта со значениями по-умлолчанию(данные клиента tcp)
+default = Dev(CLIENTS[0])
+
+# Свойства модуля
+print(device, "\n")
+
+# Информация из памяти устройства (Объект модуля)
+print(get_device_info(device))
+
+# Список сценариев устройства (Объект модуля, количество сценариев)
+[print(i) for i in read_scenarios(device, 1)]
