3f1e13953fd51e767b8bc3203d0732546247a88c
u_alex90 <u_alex90@mail.ru>
2023-07-11 21:54:26 +1000

    mod get_info in modbus.py

--- src/modbus/modbus.py
+++ src/modbus/modbus.py
@@ -1,4 +1,5 @@
 import time
+from datetime import datetime
 
 from pyModbusTCP.client import ModbusClient
 
@@ -39,11 +40,12 @@ def get_set_value(device: Dev) -> list:
 
 def get_single_holding(device: Dev, register) -> list:
     try:
-        client = ModbusClient(host=device.ip, port=device.port, unit_id=device.unit_id)
-        register = [hex(i) for i in client.read_holding_registers(register)]
+        client = ModbusClient(host=device.ip, port=device.port, unit_id=device.unit_id, timeout=5)
+        reg = [hex(i) for i in client.read_holding_registers(register)]
+        time.sleep(0.05)
         client.close()
-        if register:
-            return register
+        if reg:
+            return reg
         else:
             return ['unable to read register']
     except ValueError:
@@ -53,11 +55,27 @@ def get_single_holding(device: Dev, register) -> list:
 def get_info(device: Dev):
     """"
     Get information about the devices
+    :param device: object of the Dev
     :return: info of devices
     """
-    counter = int(get_single_holding(device, 92)[0], 16)
+    info = list()
+    try:
+        client = ModbusClient(host=device.ip, port=device.port, unit_id=device.unit_id, timeout=5)
+        info = client.read_input_registers(9000, 25)
+        time.sleep(0.05)
+        client.close()
+        if not info:
+            return 'unable to read register'
+    except ValueError:
+        print(f"Error connecting to {device.ip}:{device.port}")
 
-    return f"""Счетчик записей flash: {counter}"""
+    return f"""Тип устройства: {info[3]}
+             \rСерийный номер: {info[11]}-{info[12]}-{info[13]}-{info[14]}-{info[15]}-{info[16]} 
+             \rВерсия ПО: {info[2]}.{info[0]}{info[1]}
+             \rДата: {datetime(info[7], info[6], info[5], info[8], info[9]).strftime('%m.%d.%Y %H:%M')}
+             \rСчетчик наработки часов: {info[21]}
+             \rСчетчик записей flash: {info[22]}
+             \rКоличество переключений (~/=): {info[23]}/{info[24]}"""
 
 
 def read_scenarios(device: Dev, quantity=10) -> list:
@@ -70,7 +88,7 @@ def read_scenarios(device: Dev, quantity=10) -> list:
 
     scenarios = list()
     try:
-        client = ModbusClient(host=device.ip, port=device.port, unit_id=device.unit_id)
+        client = ModbusClient(host=device.ip, port=device.port, unit_id=device.unit_id, timeout=5)
         n = 100
         for i in range(quantity):
             regs = [hex(i) for i in client.read_holding_registers(n, 13)]
@@ -78,7 +96,7 @@ def read_scenarios(device: Dev, quantity=10) -> list:
                 scenarios.append({("Сценарий " + str(i)): regs})
             else:
                 scenarios.append({("Сценарий " + str(i)): 'unable to read registers'})
-            time.sleep(0.01)
+            time.sleep(0.05)
             n += 20
         client.close()
     except ValueError:
